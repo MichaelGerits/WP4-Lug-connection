f789df4aecee0395d0f258cca128ec8f1614ee72
Michael Gerits <michael.gerits@alchemich.be>
2024-11-25 18:06:34 +0100

    finished 4.4 and made 4.3and 4.4 functions such that they can be called again when itterating

--- Main.py
+++ Main.py
@@ -3,72 +3,88 @@ import numpy as np
 import PartDefinition as PD
 import Loads
 
-# 4.3------------------------------------------------------------------------------------------------------------------------------------
 hinge = PD.Hinge(sigmaY=250)
-Loads.F1 = F1
+Loads.F1 = F1 #TODO what does this mean?
+# 4.3------------------------------------------------------------------------------------------------------------------------------------
+def CalcLugDim(t1_init = 0.001, D1_init = 0.01):
+    #TODO: explain what you're doing here please
+    """
 
-D1 = 0.001
-w = D1
-t = 0.001 #m
-mValuesA = []
-DValuesA = []
-wValuesA = []
+    """
 
-while t < 0.25:
-    D1 = (P[1] + F1) * 1.5 / 4 / hinge.sigmaY / t #Bearing stress (1.5 is MS)
-    K_t = -0.05 * w/D1 + 3.05
-    w = (P[1] + F1) * 1.5 / 4 / (K_t * hinge.sigmaY) / t + D1 #Tension of net section
-    m = t * (w ** 2 - D1 ** 2) #this value is not actually mass, but it is proportional to mass
-    mValuesA.append(m)
-    DValuesA.append(D1)
-    wValuesA.append(w)
-    t += 0.001
+    D1 = D1_init
+    w = D1
+    t1 = t1_init #m
+    mValuesA = []
+    DValuesA = []
+    wValuesA = []
 
+    while t < 0.25:
+        D1 = (P[1] + F1) * 1.5 / 4 / hinge.sigmaY / t1 #Bearing stress (1.5 is MS)
+        K_t = -0.05 * w/D1 + 3.05
+        w = (P[1] + F1) * 1.5 / 4 / (K_t * hinge.sigmaY) / t1 + D1 #Tension of net section
+        m = t1 * (w ** 2 - D1 ** 2) #this value is not actually mass, but it is proportional to mass
+        mValuesA.append(m)
+        DValuesA.append(D1)
+        wValuesA.append(w)
+        t1 += 0.001
 
-D1 = 0.001
-w = D1
-t = 0.001 #m
-mValuesB = []
-DValuesB = []
-wValuesB = []
+    #TODO: comment why the same this has been done twice please
+    """
 
-while t < 0.25:
-    A_frac = 6 / (D1 * (4/(0.5*w-m.sqrt(2)*0.25 * D1) + 2/(0.5*(w-D1)))) # p18 fig D1.15
-    K_bending = 1.2143 * A_frac #Fig D1.15 page 18
-    D = Loads.P[0] / 4 / (hinge.K_bending * hinge.sigmaY) / t #Bending
-    m = t * (w ** 2 - D1 ** 2) #this value is not actually mass, but it is proportional to mass
-    mValuesB.append(m)
-    DValuesB.append(D1)
-    wValuesB.append(w)
-    t += 0.001
+    """
 
-mMinList = [min(mValuesA), min(mValuesB)]
-mMax = max(mMinList)
-mMaxIndex = mMinList.index(mMax)
-if mMaxIndex = 0:
-    mMinIndex = mValuesA.index(mMax)
-    t = mMinIndex * 0.001 + 0.001
-    D1 = DValuesA[mMinIndex]  # Bearing stress (1.5 is MS)
-    w = wValuesA[mMinIndex]
-else:
-    mMinIndex = mValuesB.index(mMax)
-    t = mMinIndex * 0.001 + 0.001
-    D1 = DValuesB[mMinIndex]  # Bearing stress (1.5 is MS)
-    w = wValuesB[mMinIndex]
+    D1 = D1_init
+    w = D1
+    t1 = t1_init #m
+    mValuesB = []
+    DValuesB = []
+    wValuesB = []
 
-print(f"t = {t}", f"hole diameter = {D1}", f"width = {w}")
+    while t1 < 0.25:
+        A_frac = 6 / (D1 * (4/(0.5*w-m.sqrt(2)*0.25 * D1) + 2/(0.5*(w-D1)))) # p18 fig D1.15
+        K_bending = 1.2143 * A_frac #Fig D1.15 page 18
+        D = Loads.P[0] / 4 / (hinge.K_bending * hinge.sigmaY) / t #Bending
+        m = t1 * (w ** 2 - D1 ** 2) #this value is not actually mass, but it is proportional to mass
+        mValuesB.append(m)
+        DValuesB.append(D1)
+        wValuesB.append(w)
+        t1 += 0.001
+
+    mMinList = [min(mValuesA), min(mValuesB)]
+    mMax = max(mMinList)
+    mMaxIndex = mMinList.index(mMax)
+    if mMaxIndex == 0:
+        mMinIndex = mValuesA.index(mMax)
+        t = mMinIndex * 0.001 + 0.001
+        D1 = DValuesA[mMinIndex]  # Bearing stress (1.5 is MS)
+        w = wValuesA[mMinIndex]
+    else:
+        mMinIndex = mValuesB.index(mMax)
+        t1 = mMinIndex * 0.001 + 0.001
+        D1 = DValuesB[mMinIndex]  # Bearing stress (1.5 is MS)
+        w = wValuesB[mMinIndex]
+
+    #updates the new values to the hinge object
+    hinge.w = w
+    hinge.D1 = D1
+    hinge.t1 = t1
+    print(f"t = {t1}", f"hole diameter = {D1}", f"width = {w}")
 #-------------------------------------------------------------------------------------------------------------------------------------------
+#runs the function for the first time
+CalcLugDim()
 
-#4.4
-"""
-calculates the dimensions of the baseplate with the initial hole dimension 
-"""
+#4.4-----------------------------------------------------------------------------------------------------------------
+def CalcBasePlateDim(e1Fac=1.5, e2Fac=1.5, holeSepFac=2.5):
+    """
+    calculates the dimensions of the baseplate with the initial hole dimension 
+    """
 
-holeSepFac = 2.5
+    hinge.D2 = hinge.w/(2+ 2 * e1Fac + holeSepFac)
+    hinge.e1 = e1Fac*hinge.D2
+    hinge.e2 = e2Fac*hinge.D2
 
-hinge.w = w #TODO: update this with Anwita's calculations
-hinge.D2 = 
-hinge.e2 = 1.5*hinge.D2
-
-
-hinge.depth = 2*(2* hinge.e2 + hinge.t1) + hinge.h
+    hinge.depth = 2*(2* hinge.e2 + hinge.t1) + hinge.h
+#---------------------------------------------------------------------------------------------------------------------------
+#runs the function for the first time
+CalcBasePlateDim()
