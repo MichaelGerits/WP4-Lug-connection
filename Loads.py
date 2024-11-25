ef54cebf516422ebb2cf5a24ff1bcb46759fa258
SilverBirdie <anwitagadgil1@gmail.com>
2024-11-25 16:02:22 +0100

    another test on pycharm

--- Loads.py
+++ Loads.py
@@ -11,3 +11,7 @@ P = np.zeros(3) #if a component is zero, take 10% of the total
 #resulting Moment [Mx, My, Mz]
 T = np.zeros(3)
 
+F1 = T[0] * H / 2
+
+
+
--- Main.py
+++ Main.py
@@ -1,3 +1,9 @@
 import math as m
 import numpy as np
-import PartDefinition as PD 
+import PartDefinition as PD
+import Loads
+
+# 4.3
+hinge = PD.Hinge()
+Loads.F1 = F1
+
