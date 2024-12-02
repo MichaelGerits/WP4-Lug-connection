138c3ba206817751a91b290cc74b3f3ec0babccd
SilverBirdie <anwitagadgil1@gmail.com>
2024-12-02 16:09:09 +0100

    Used scipy instead of rawdogging it Three is real, the other two CalcLugDims are forsaken by the gods

--- Itteration.py
+++ Itteration.py
@@ -1,12 +1,14 @@
 import Main
 import PartDefinition as PD
+import scipy
 import Loads
 
 #initial definition of the hinge obect
-hinge = PD.Hinge(t1=0.001, D1=0.01, w=0.02 ,sigmaY=250000000)
+hinge = PD.Hinge(t1=0.0011, D1=0.0011, w=0.0021, sigmaY=414000000)
 
 #runs the functions for the first time
-Main.CalcLugDim(hinge)
-Main.CalcBasePlateDim(hinge)
-Fasteners = Main.CalcFastenerPos(hinge)
-FastCG = Main.CalcCG(Fasteners)
+print(scipy.optimize.minimize(Main.CalcLugDimThree, [0.01, 0.01, 0.02], bounds=scipy.optimize.Bounds([0.001, 0.001, 0.002], [0.25, 0.5, 0.5])))
+# Main.CalcLugDimTwo(hinge)
+# Main.CalcBasePlateDim(hinge)
+# Fasteners = Main.CalcFastenerPos(hinge)
+# FastCG = Main.CalcCG(Fasteners)
